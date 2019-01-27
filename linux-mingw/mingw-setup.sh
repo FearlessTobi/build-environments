f082c325d8b6f2d601b3d5070974e897774f0297
liushuyu <liushuyu011@gmail.com>
2019-01-27 09:15:46 +0800

    linux-mingw: re-add ffmpeg

--- linux-mingw/mingw-setup.sh
+++ linux-mingw/mingw-setup.sh
@@ -10,6 +10,19 @@ wget -q "${MINGW_URL}" -O 'mingw.7z'
 7z x 'mingw.7z' "${TARGET_DIR}"lib{mf,mfplat,mfuuid}.a
 cp -rv "${TARGET_DIR}"/* '/usr/x86_64-w64-mingw32/lib/'
 
+# ffmpeg
+FFMPEG_VER='4.1'
+for i in 'shared' 'dev'; do
+  echo "Downloading ffmpeg (${i})..."
+  wget -q -c "https://ffmpeg.zeranoe.com/builds/win64/${i}/ffmpeg-${FFMPEG_VER}-win64-${i}.zip"
+  7z x "ffmpeg-${FFMPEG_VER}-win64-${i}.zip" > /dev/null
+done
+
+echo "Copying ffmpeg ${FFMPEG_VER} files to sysroot..."
+cp -v "ffmpeg-${FFMPEG_VER}-win64-shared"/bin/*.dll /usr/x86_64-w64-mingw32/lib/
+cp -vr "ffmpeg-${FFMPEG_VER}-win64-dev"/include /usr/x86_64-w64-mingw32/
+cp -v "ffmpeg-${FFMPEG_VER}-win64-dev"/lib/*.{a,def} /usr/x86_64-w64-mingw32/lib/
+
 # remove the directory
 ABS_PATH="$(readlink -f $0)"
 rm -rf "$(dirname ${ABS_PATH})"
