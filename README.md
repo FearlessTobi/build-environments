8a43699577f211cb6b199ad7656ed38cbdff9b14
chris062689 <chris062689@gmail.com>
2019-03-22 20:27:11 -0400

    Initial addition of build-environment files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# build-environments
+Dockerfile entries used for building yuzu binaries.
+
+https://hub.docker.com/r/yuzuemu/yuzu-build-environments/
---
+++ linux-mingw/mingw-setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash -e
+# install pefile
+pip3 install pefile
+
+MINGW_URL='https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/8.1.0/threads-posix/seh/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z/download'
+TARGET_DIR='mingw64/x86_64-w64-mingw32/lib/'
+
+echo 'Downloading MinGW replacement binaries...'
+wget -q "${MINGW_URL}" -O 'mingw.7z'
+7z x 'mingw.7z' "${TARGET_DIR}"lib{mf,mfplat,mfuuid}.a
+cp -rv "${TARGET_DIR}"/* '/usr/x86_64-w64-mingw32/lib/'
+
+# ffmpeg
+FFMPEG_VER='4.1'
+for i in 'shared' 'dev'; do
+  echo "Downloading ffmpeg (${i})..."
+  wget -q -c "https://ffmpeg.zeranoe.com/builds/win64/${i}/ffmpeg-${FFMPEG_VER}-win64-${i}.zip"
+  7z x "ffmpeg-${FFMPEG_VER}-win64-${i}.zip" > /dev/null
+done
+
+echo "Copying ffmpeg ${FFMPEG_VER} files to sysroot..."
+cp -v "ffmpeg-${FFMPEG_VER}-win64-shared"/bin/*.dll /usr/x86_64-w64-mingw32/lib/
+cp -vr "ffmpeg-${FFMPEG_VER}-win64-dev"/include /usr/x86_64-w64-mingw32/
+cp -v "ffmpeg-${FFMPEG_VER}-win64-dev"/lib/*.{a,def} /usr/x86_64-w64-mingw32/lib/
+
+# remove the directory
+ABS_PATH="$(readlink -f $0)"
+rm -rf "$(dirname ${ABS_PATH})"
