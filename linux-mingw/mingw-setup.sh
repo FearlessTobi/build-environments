dc54c22e113034304de2a5a04ca397ad169205d3
FearlessTobi <thm.frey@gmail.com>
2020-04-04 19:10:55 +0200

    Set mingw SDL2 version to 2.0.10
    Update the SDL2 version of the mingw builds, as it is necessary for better controller support. See citra-emu/citra#5100.
    
    Co-Authored-By: Vitor K <vitor-k@users.noreply.github.com>

--- linux-mingw/mingw-setup.sh
+++ linux-mingw/mingw-setup.sh
@@ -10,6 +10,14 @@ wget -q "${MINGW_URL}" -O 'mingw.7z'
 7z x 'mingw.7z' "${TARGET_DIR}"lib{mf,mfplat,mfuuid}.a
 cp -rv "${TARGET_DIR}"/* '/usr/x86_64-w64-mingw32/lib/'
 
+# SDL2
+SDL2_VER='2.0.10'
+wget "https://www.libsdl.org/release/SDL2-devel-${SDL2_VER}-mingw.tar.gz"
+tar -zxf "SDL2-devel-${SDL2_VER}-mingw.tar.gz"
+cd SDL2-${SDL2_VER}/
+make install-package arch=x86_64-w64-mingw32 prefix=/usr/x86_64-w64-mingw32;
+cd ..
+
 # ffmpeg
 FFMPEG_VER='4.1'
 for i in 'shared' 'dev'; do
